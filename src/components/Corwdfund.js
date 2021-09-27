90638b95ed5ecc9a6dc254c914b5873978e41529
Alexander Chi <arturochi2@hotmail.com>
2021-09-27 13:31:38 -0500

    Se agregó components/Header.js

--- src/components/Corwdfund.js
+++ src/components/Corwdfund.js
@@ -1,11 +1,24 @@
+/**
+ * @author: alexandercd
+ * @date: 27/septiembre/2021
+ * @description: 
+**/
 import React from 'react';
+import Header from './Header';
 
 const Corwdfund = () => {
     return (
         <>
-            <h2>Hello</h2>
-            <p>chindrens of technology</p>
-            <spn>Like, Comment, Share</spn>
+            <Header></Header>
+            <div className="container">
+                <div className="card">
+
+                </div>
+                <div className="card">
+
+                </div>
+                
+            </div>
         </>
     );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+/**
+ * @author: alexandercd
+ * @date: 27/septiembre/2021
+ * @description: 
+**/
+import React from 'react';
+import logo from '../assets/svgs/logo.svg'; 
+import PropTypes from 'prop-types';
+
+const items = [
+    {
+        name: 'About',
+        to: 'about'
+    },
+    {
+        name: 'Discover',
+        to: 'discover'
+    },
+    {
+        name: 'Get Started',
+        to: 'get-started'
+    },
+]
+
+const Header = ({title, src, width, height}) => {
+    return (
+        <header>
+            <div>
+                <img 
+                    alt={title}
+                    src={src}
+                    width={width}
+                    height={height}
+                ></img>
+            </div>
+            <div>
+                <ul>
+                    {items.map( (item, index) => (
+                        <li key={item.to}>
+                            <a href={`#${item.to}`}>{item.name}</a>
+                        </li>
+                    ) )}
+                </ul>
+            </div>
+        </header>
+    );
+}
+
+Header.defaultProps = {
+    title: 'CrowdFund',
+    width: '130',
+    height: '20',
+    src: logo
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header;
